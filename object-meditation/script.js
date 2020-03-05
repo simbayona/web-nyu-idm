1d0fed621d67b9ae82f70c4bb5288e13cfdae0d9
simbayona <60442765+simbayona@users.noreply.github.com>
2020-03-05 16:16:21 -0500

    buttons working, spacing edits, transition position fixed

--- object-meditation/script.js
+++ object-meditation/script.js
@@ -1,1 +1,18 @@
-console.log("I'm here, I'm here!!")
+console.log("I'm here, I'm here!!");
+
+function huggableChallenge() {
+  $("#huggable").css("display", "block");
+  $("#lovable").css("display", "none");
+  $("#comical").css("display", "none");
+}
+function lovableChallenge() {
+  $("#huggable").css("display", "none");
+  $("#lovable").css("display", "block");
+  $("#comical").css("display", "none");
+}
+function comicalChallenge() {
+  $("#huggable").css("display", "none");
+  $("#lovable").css("display", "none");
+  $("#comical").css("display", "block");
+
+}
